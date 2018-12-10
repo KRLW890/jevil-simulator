4607f4ff3c2c44dbf413e5c7e40a9d7db26d1b58
samsamson33 <quinnydf@yahoo.ca>
2018-12-10 13:09:22 -0500

    clarity

--- image_handler.js
+++ image_handler.js
@@ -1,5 +1,4 @@
-var Animation = function(spritesheet, frames)
-{
+var Animation = function(spritesheet, frames) {
     this.sprites = new Image();
     this.sprites.src = spritesheet;
     this.frames = frames;
@@ -11,18 +10,21 @@ var Animation = function(spritesheet, frames)
 
 // plays the animation. It can either loop or stop on the last frame
 // if it is not set to loop, it returns true when the last frame has been played
-Animation.prototype.play = function(x, y, loop, framerate, w, h)
-{
-    if (framerate == null)
-        framerate = 1; // the speed of the animation is divided by framerate
+// the speed of the animation is divided by framerate
+Animation.prototype.play = function(x, y, loop, framerate, w, h) {
+    if (framerate == null) 
+    {
+        framerate = 1; 
+    }
     if (w == null)
     {
         w = this.width;
         h = this.height;
     }
     
-    if (this.playing === false) // reset the frame counter if it's the first frame
+    if (this.playing === false) 
     {
+        // reset the frame counter if it's the first frame
         this.frameCount = 0;
         this.playing = true;
     }
@@ -30,14 +32,17 @@ Animation.prototype.play = function(x, y, loop, framerate, w, h)
     ctx.drawImage(this.sprites, this.width*(Math.floor(this.frameCount / framerate)%this.frames), 0, this.width, this.height, x, y, w, h);
     
     if (loop || this.frameCount < this.frames-1)
+    {
         this.frameCount++;
-    else // if the function is not set to loop and has reached the last frame, return true
+    } else 
+    {
+        // if the function is not set to loop and has reached the last frame, return true
         return true;
+    }
 };
 
 // draws a specific frame of the animation
-Animation.prototype.drawFrame = function(x, y, frame, w, h)
-{
+Animation.prototype.drawFrame = function(x, y, frame, w, h) {
     if (w == null)
     {
         w = this.width;
