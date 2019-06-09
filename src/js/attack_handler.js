baa1a2cb0d6be0af652b17b137de07e8d5fd0150
samsamson33 <35579293+samsamson33@users.noreply.github.com>
2019-06-09 12:16:19 -0400

    Make `Attack` an ES6 class

--- src/js/attack_handler.js
+++ src/js/attack_handler.js
@@ -4,7 +4,7 @@ var attackData = {
     bullets: [],
     duration: 0, // number of frames until the attack ends
     target: 0, // 0-2 means it only targets one character, 3 means it targets everyone
-    
+
     playerX: 320,
     playerY: 175,
     iFrames: 0, // number of invincibility frames the player currently has
@@ -16,7 +16,7 @@ var executeAttack = function() {
     fill(0);
     strokeWeight(4);
     rect(248, 98, 146, 146); // battle box
-    
+
     if (keys.pressed(keys.up) && attackData.playerY > 108) {
             attackData.playerY -= 4;
             if (attackData.playerY < 108)
@@ -37,7 +37,7 @@ var executeAttack = function() {
             if (attackData.playerX > 384)
                 attackData.playerX = 384;
     }
-    
+
     if (attackData.collision) // for debugging
         animations.playerSoul.drawFrame(attackData.playerX-8, attackData.playerY-8, 1);
     else if (attackData.iFrames > 0) {
@@ -46,9 +46,9 @@ var executeAttack = function() {
     }
     else
         animations.playerSoul.drawFrame(attackData.playerX-8, attackData.playerY-8, 0);
-    
+
     attacks[attackData.id].spawnBullets();
-    
+
     attackData.collision = false; // for debugging
     for (var i = 0; i < attackData.bullets.length; i++) {
         if (attackData.bullets[i] != null) {
@@ -56,41 +56,44 @@ var executeAttack = function() {
                 attackData.bullets[i] = null;
         }
     }
-    
+
     attackData.duration--;
     if (attackData.duration <= 0)
         turnPhase++;
 };
 
 
-var Attack = function(baseDamage, isMultiTarget, duration) {
-    this.baseDamage = baseDamage;
-    this.isMultiTarget = isMultiTarget;
-    this.duration = duration || 300; // how many frames the attack lasts
-    this.spawnBullets = function() {}; // will be filled in later. This function determines which bullets are spawned and when
-};
-Attack.prototype.prepareAttack = function() {
-    attackData.baseDamage = this.baseDamage;
-    attackData.duration = this.duration;
-    if (this.isMultiTarget)
-        attackData.target = 3;
-    else
-        attackData.target = Math.floor(Math.random()*3);
-    
-    attackData.playerX = 320;
-    attackData.playerY = 175;
-};
+class Attack {
+    constructor(baseDamage, isMultiTarget, duration) {
+        this.baseDamage = baseDamage;
+        this.isMultiTarget = isMultiTarget;
+        this.duration = duration || 300; // how many frames the attack lasts
+        this.spawnBullets = function() {}; // will be filled in later. This function determines which bullets are spawned and when
+    }
+
+    prepareAttack() {
+        attackData.baseDamage = this.baseDamage;
+        attackData.duration = this.duration;
+        if (this.isMultiTarget)
+            attackData.target = 3;
+        else
+            attackData.target = Math.floor(Math.random()*3);
+
+        attackData.playerX = 320;
+        attackData.playerY = 175;
+    }
+}
 
 var addBullet = function(bullet) { // should be called as addBullet(new BulletType());
     var i = 0;
     while (attackData.bullets[i] != null)
         i++;
-    
+
     attackData.bullets[i] = bullet;
 };
 
 var attacks = [
-    new Attack(50, false), // tiny "ohp" spades 
+    new Attack(50, false), // tiny "ohp" spades
     new Attack(50, false), // big spade ringaround 1
     new Attack(40, true),  // heart bombs
     new Attack(60, false), // devilsknife 1
@@ -112,5 +115,3 @@ attacks[2].spawnBullets = function() {
     if (attackData.duration % 25 == 0)
         addBullet(new HeartBomb());
 };
-
-
